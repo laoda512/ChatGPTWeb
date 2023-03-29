e62b390665265b7ca4ca91b1ff1879c10bf93a01
laoda512 <1176855+laoda512@users.noreply.github.com>
2023-03-29 15:03:57 +0800

    Update index.ts

--- service/src/index.ts
+++ service/src/index.ts
@@ -5,6 +5,8 @@ import { chatConfig, chatReplyProcess, currentModel } from './chatgpt'
 import { auth } from './middleware/auth'
 import { limiter } from './middleware/limiter'
 import { isNotEmptyString } from './utils/is'
+import './consoleLogger';
+
 
 const app = express()
 const router = express.Router()
